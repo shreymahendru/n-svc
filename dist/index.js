c8d0b19e4cb94effc139f1bf82f39aff538aa0e3
nivinjoseph <nivinjoseph@outlook.com>
2022-01-23 10:10:14 -0500

    preparing to publish new version

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SvcApp = void 0;
+require("@nivinjoseph/n-ext");
 const svc_app_1 = require("./svc-app");
 Object.defineProperty(exports, "SvcApp", { enumerable: true, get: function () { return svc_app_1.SvcApp; } });
 //# sourceMappingURL=index.js.map
