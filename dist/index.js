cec3aefbb16bdb2043a43966c686b2fc695a3f30
Nivin Joseph <nivinjoseph@outlook.com>
2024-01-05 00:59:30 -0500

    preparing to publish new version

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ShutdownManager = exports.SvcApp = void 0;
-require("@nivinjoseph/n-ext");
-const svc_app_1 = require("./svc-app");
-Object.defineProperty(exports, "SvcApp", { enumerable: true, get: function () { return svc_app_1.SvcApp; } });
-const shutdown_manager_1 = require("./shutdown-manager");
-Object.defineProperty(exports, "ShutdownManager", { enumerable: true, get: function () { return shutdown_manager_1.ShutdownManager; } });
+import "@nivinjoseph/n-ext";
+import { SvcApp } from "./svc-app.js";
+import { ShutdownManager } from "./shutdown-manager.js";
+export { SvcApp, ShutdownManager };
 //# sourceMappingURL=index.js.map
--- dist/program.d.ts
+++ dist/program.d.ts
@@ -2,3 +2,4 @@ export interface Program {
     start(): Promise<void>;
     stop(): Promise<void>;
 }
+//# sourceMappingURL=program.d.ts.map
